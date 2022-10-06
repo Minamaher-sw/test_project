e7abbe115764665a0bf51488ea8135e6ed35c038
Mina Maher <menamosadef5@gmail.com>
2022-10-06 11:58:41 +0200

    ADD DISPLAY FUNCTION

---
+++ main.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include <stdlib.h> //for malloc & free functions
+
+struct node{
+	int data;
+	struct node*next;
+
+};
+
+struct node *head=NULL;
+
+void insert_Linked_list(int value)
+{
+
+	  struct node*temp=(struct node*)malloc(sizeof(struct node));
+	  temp->data=value;
+	  temp->next= head ;
+	  head=temp;
+
+}
+
+void delet_Linked_list()
+{
+      struct node* temp=head;
+	   head=head->next;
+	   free(temp);
+
+}
+void display(void)
+{
+   while(head !=NULL)
+   {
+	   printf("%d \n",head->data);
+	   head=head->next;
+   }
+}
+int main()
+{
+	setbuf(stdout,NULL);
+	insert_Linked_list(20);
+	insert_Linked_list(30);
+	insert_Linked_list(40);
+	insert_Linked_list(50);
+	insert_Linked_list(60);
+	insert_Linked_list(70);
+	delet_Linked_list();
+	delet_Linked_list();
+	display();
+
+}
